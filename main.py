bf93e8b52cdfef8062d47b2dd7561555b3ed8214
Maciej Gajda <gajdam11@gmail.com>
2024-03-31 21:53:50 +0200

    PasswordManager class added
    custom way of storing passwords in sqlite db

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sqlite3
+import hashlib
+import os
+import secrets
+import hmac
+from hashlib import pbkdf2_hmac
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class PasswordManager:
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS passwords
+                            (id INTEGER PRIMARY KEY, hash TEXT, salt TEXT)''')
+        self.conn.commit()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def _generate_salt(self):
+        return secrets.token_hex(16)
+
+    def _hash_password(self, password, salt):
+        hashed_password = hashlib.sha256((password + salt).encode()).hexdigest()
+        return hashed_password
+
+    def store_password(self, password):
+        salt = self._generate_salt()
+        hashed_password = self._hash_password(password, salt)
+        self.cursor.execute("INSERT INTO passwords (hash, salt) VALUES (?, ?)", (hashed_password, salt))
+        self.conn.commit()
+
+    def verify_password(self, password):
+        self.cursor.execute("SELECT hash, salt FROM passwords")
+        stored_data = self.cursor.fetchone()
+        if stored_data:
+            stored_hash, salt = stored_data
+            hashed_password = self._hash_password(password, salt)
+            return hmac.compare_digest(hashed_password, stored_hash)
+        return False
+
+# Przykładowe użycie:
+manager = PasswordManager("passwords.db")
+manager.store_password("secret_password")
+print(manager.verify_password("secret_password"))
